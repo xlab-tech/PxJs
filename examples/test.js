e0b7bddd39b024bbadf35e2aeefb2d6cc2c8b970
victor <vrubinat@gmail.com>
2019-01-04 21:52:18 +0100

    fix concatCurry

--- examples/test.js
+++ examples/test.js
@@ -9,8 +9,9 @@ const pipe2 = Px
   .chain(input => 'Branch 2');
 
 Px.chain(input => `${input} Chain`)
-   .branch('t', pipe, 't2', pipe2)
+  //.branch('t', pipe, 't2', pipe2)
   //.branch(pipe, pipe2)
+  .concatCurry(() => [pipe, pipe2])
   // .branchIf(() => false, pipe)
   // .chain(input => `${input} TWO`)
   .subscribe(res => console.log(res), err => console.log(err))
